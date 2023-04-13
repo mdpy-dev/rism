2d8769bc19ae26b7f891abc7ccdc8b9b73a0b1c2
zhenyuwei99 <zhenyuwei99@gmail.com>
2023-04-13 12:43:17 +0800

    Update test code for rism.potential package

--- rism/test/test_rvdw.py
+++ rism/test/test_rvdw.py
@@ -2,17 +2,49 @@
 # -*- encoding: utf-8 -*-
 """
 file : test_rvdw.py
-created time : 2023/04/03
+created time : 2023/04/13
 author : Zhenyu Wei
-version : 1.0
-contact : zhenyuwei99@gmail.com
-copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+copyright : (C)Copyright 2021-present, mdpy organization
 """
 
+
+import pytest
 import cupy as cp
+import numpy as np
 import matplotlib.pyplot as plt
-from rism.unit import *
+from rism.environment import CUPY_FLOAT
 from rism.potential import RVDWPotential
+from rism.unit import *
+from rism.error import *
+
+
+class TestRVDWPotential:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_attributes(self):
+        u = RVDWPotential(type1="k", type2="c")
+        assert u.type1 == "k"
+        assert u.type2 == "c"
+
+    def test_exceptions(self):
+        with pytest.raises(UnregisteredParticleError):
+            RVDWPotential(type1="ab", type2="c")
+
+    def test_evaluate(self):
+        threshold = Quantity(1, kilocalorie_permol)
+        vdw = RVDWPotential(type1="c", type2="k")
+        r = cp.arange(0, 10, 0.1)
+        u = vdw.evaluate(r, threshold=threshold)
+        assert isinstance(u, cp.ndarray)
+        assert u.dtype == CUPY_FLOAT
+        assert np.isclose(
+            u.max().get(), threshold.convert_to(default_energy_unit).value
+        )
+
 
 if __name__ == "__main__":
 
