5f5bf7404652d6a60388d3eb8737dcd67f7ad149
zhenyuwei99 <zhenyuwei99@gmail.com>
2023-04-24 10:50:31 +0800

    Add rism.solver.RISMSolventPicard1DSolver class

--- rism/solver/__init__.py
+++ rism/solver/__init__.py
@@ -4,5 +4,12 @@ __copyright__ = "(C)Copyright 2021-present, mdpy organization"
 __license__ = "BSD-3"
 
 
+# 1D OZ
+from rism.solver.oz_solvent_picard_1d import OZSolventPicard1DSolver
+
+# 3D OZ
 from rism.solver.oz_solvent_picard_3d import OZSolventPicard3DSolver
 from rism.solver.oz_solvent_nr_3d import OZSolventNR3DSSolver
+
+# 1D RISM
+from rism.solver.rism_solvent_picard_1d import RISMSolventPicard1DSolver
