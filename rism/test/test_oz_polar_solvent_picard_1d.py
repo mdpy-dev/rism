83ca2daa3acb64047b60d9a92b1e1a913318c9a5
zhenyuwei99 <zhenyuwei99@gmail.com>
2023-04-25 17:09:29 +0800

    Add test for rism.solver.OZPolarSolventPicard1DSolver class

--- rism/test/test_oz_polar_solvent_picard_1d.py
+++ rism/test/test_oz_polar_solvent_picard_1d.py
@@ -14,13 +14,16 @@ import matplotlib
 import matplotlib.pyplot as plt
 import rism
 from rism.core import FFTGrid
-from rism.solver.oz_solvent_picard_1d import OZSolventPicard1DSolver
+from rism.solver.oz_polar_solvent_picard_1d import OZPolarSolventPicard1DSolver
 from rism.environment import CUPY_FLOAT
 from rism.unit import *
 
 
-def visualize(grid, h, c):
-    fig, ax = plt.subplots(1, 3, figsize=[25, 9])
+def visualize(grid, h, c, plt_arg=None):
+    if plt_arg is None:
+        fig, ax = plt.subplots(1, 3, figsize=[25, 9])
+    else:
+        fig, ax = plt_arg
     g = h + 1
     gamma = h - c
 
@@ -31,23 +34,34 @@ def visualize(grid, h, c):
     ax[0].set_title("g")
     ax[1].set_title("c")
     ax[2].set_title(r"$\gamma$")
-    plt.show()
+    return fig, ax
 
 
 if __name__ == "__main__":
     temperature = 300
-    grid = FFTGrid(r=[0, 20, 512])
+    grid = FFTGrid(r=[0, 25, 2048])
     rho_b = Quantity(1.014, kilogram / decimeter**3) / Quantity(18, dalton) / NA
     closure = rism.closure.hnc
 
-    solver = OZSolventPicard1DSolver(
+    solver = OZPolarSolventPicard1DSolver(
         grid=grid,
         closure=closure,
         temperature=temperature,
-        solvent_type="o",
+        solvent_type="h",
         rho_b=rho_b,
     )
-    h, c = solver.solve(max_iterations=500, error_tolerance=1e-5)
-    visualize(grid, h, c)
+    h, c = solver.solve(
+        max_iterations=1000, error_tolerance=1e-5, log_freq=50, alpha=1.0
+    )
+    fig, ax = visualize(grid, h, c)
+    h, c = solver.solve(
+        max_iterations=1000, error_tolerance=1e-5, log_freq=50, alpha=0.8
+    )
+    visualize(grid, h, c, (fig, ax))
+    h, c = solver.solve(
+        max_iterations=1000, error_tolerance=1e-5, log_freq=50, alpha=1.1
+    )
+    visualize(grid, h, c, (fig, ax))
+    plt.show()
     # h, c = solver.solve(np.array([0, 0, 0]), iterations=500, restart_value=(h, c))
     # visualize(grid, h, c, False)
