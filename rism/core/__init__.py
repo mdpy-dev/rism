c871840c96239cfb0ac0e6eea8ceb56e0942e649
zhenyuwei99 <zhenyuwei99@gmail.com>
2023-05-13 09:24:25 +0800

    Add rism.core.Particle class

--- rism/core/__init__.py
+++ rism/core/__init__.py
@@ -3,6 +3,9 @@ __maintainer__ = "Zhenyu Wei"
 __copyright__ = "(C)Copyright 2021-present, mdpy organization"
 __license__ = "BSD-3"
 
-from rism.core.fft_grid import FFTGrid
+from rism.core.particle import Particle
 from rism.core.solvent import Solvent
+
+
+from rism.core.fft_grid import FFTGrid
 from rism.core.matrix import Matrix
