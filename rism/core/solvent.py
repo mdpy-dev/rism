042a3c915bcc13903988b969ae0f4fb50340646f
zhenyuwei99 <zhenyuwei99@gmail.com>
2023-05-13 12:07:14 +0800

    Update rism.core.Solvent class: Update for rism.core.Parctile class

--- rism/core/solvent.py
+++ rism/core/solvent.py
@@ -7,104 +7,97 @@ author : Zhenyu Wei
 copyright : (C)Copyright 2021-present, mdpy organization
 """
 
-import json
-from rism.potential import check_particle_type, FF_DICT
+
+from collections import Counter
+from rism.core import Particle
+from rism.error import DuplicatedNameError, SolventIncompleteError
 from rism.unit import *
 
 
 class Solvent:
-    def __init__(self, solvent_file_path: str = None) -> None:
-        if solvent_file_path is None:
-            self._num_particles = 0
-            self._num_bonds = 0
-            self._topology = {"particle": {}, "bond": {}}
+    def __init__(self, particle_list: list[Particle]) -> None:
+        self._particle_list = particle_list
+        self._bond_dict = {}
+        self._name_list = [i.name for i in self._particle_list]
+        self._num_particles = len(self._name_list)
+        if len(set(self._name_list)) != self._name_list:
+            count = dict(Counter(self._name_list))
+            for key, val in count.items():
+                if val != 1:
+                    raise DuplicatedNameError("Duplicate particle name %s" % key)
+
+    def _parse_index(self, index):
+        if isinstance(index, str):
+            return self._name_list.index(index)
+        elif isinstance(index, Particle):
+            return self._name_list.index(index.name)
+        elif isinstance(index, int):
+            if index >= self._num_particles:
+                raise IndexError("The site index out of range")
+            return index
         else:
-            with open(solvent_file_path, "r") as f:
-                self._topology = json.load(f)
-            self._num_particles = len(self.particle_list)
-            self._num_bonds = len(self.bond_list)
+            raise KeyError("Only int, str, Particle are supported argument")
 
-    def _exists_particle(self, name):
-        if name in self.particle_list:
-            return True
-        return False
+    def _get_bond_name(self, index1, index2):
+        index1 = self._parse_index(index1)
+        index2 = self._parse_index(index2)
+        if index1 > index2:
+            index1, index2 = index2, index1
+        bond_name = "%s-%s" % (self._name_list[index1], self._name_list[index2])
+        return bond_name
 
-    def _check_particle(self, name):
-        if not self._exists_particle(name):
-            raise KeyError("Particle %s does not exists in the topology" % name)
-
-    def add_particle(self, name, particle_type, particle_params: dict = None):
-        particle_type = check_particle_type(particle_type)
-        if not self._exists_particle(name) or not particle_params is None:
-            cur_particle = {"type": particle_type}
-            if particle_params is None:
-                particle_params = FF_DICT[particle_type]
-            cur_particle["sigma"] = float(
-                check_quantity_value(particle_params["sigma"], default_length_unit)
-            )
-            cur_particle["epsilon"] = float(
-                check_quantity_value(particle_params["epsilon"], default_energy_unit)
-            )
-            cur_particle["q"] = float(
-                check_quantity_value(particle_params["q"], default_charge_unit)
-            )
-            self._topology["particle"][name] = cur_particle
-        self._num_particles = len(self.particle_list)
-
-    def get_particle(self, name):
-        self._check_particle(name)
-        return self._topology["particle"][name]
-
-    def _exists_bond(self, name1, name2):
-        cur_bond_name = self._topology["bond"].keys()
-        bond_names = ["%s-%s" % (name1, name2)]
-        bond_names += ["%s-%s" % (name2, name1)]
-        for bond_name in bond_names:
-            if bond_name in cur_bond_name:
-                return True, bond_name
-        return False, bond_name
-
-    def add_bond(self, name1, name2, bond_length):
-        self._check_particle(name1)
-        self._check_particle(name2)
-        bond_name = "%s-%s" % (name1, name2)
-        if not self._exists_bond(name1, name2)[0]:
-            self._topology["bond"][bond_name] = float(
+    def add_bond(self, index1, index2, bond_length):
+        bond_name = self._get_bond_name(index1, index2)
+        if not bond_name in self._bond_dict.keys():
+            self._bond_dict[bond_name] = float(
                 check_quantity_value(bond_length, default_length_unit)
             )
-        self._num_bonds = len(self.bond_list)
 
-    def get_bond(self, name1, name2):
-        is_exist, bond_name = self._exists_bond(name1, name2)
-        if is_exist:
-            return self._topology["bond"][bond_name]
+    def get_particle(self, index):
+        index = self._parse_index(index)
+        return self._particle_list[index]
+
+    def get_bond_length(self, index1, index2):
+        bond_name = self._get_bond_name(index1, index2)
+        if bond_name in self._bond_dict.keys():
+            return self._bond_dict[bond_name]
         else:
-            raise KeyError(
-                "Bond %s-%s does not exists in the topology" % (name1, name2)
-            )
+            raise KeyError("Bond %s does not exists in the topology" % bond_name)
 
-    def save(self, solvent_file_path: str):
-        with open(solvent_file_path, "w") as f:
-            data = json.dumps(self._topology, sort_keys=True, indent=4)
-            data = data.encode("utf-8").decode("unicode_escape")
-            print(data, file=f)
+    def check_completeness(self):
+        bond_list = list(self._bond_dict.keys())
+        is_complete = True
+        for i in range(self._num_particles):
+            for j in range(i, self._num_particles):
+                bond_name = self._get_bond_name(i, j)
+                if not bond_name in bond_list:
+                    is_complete = False
+                    break
+        if not is_complete:
+            raise SolventIncompleteError("Solvent missing %s bond" % bond_name)
 
-    @property
-    def topology(self) -> dict:
-        return self._topology
+    def asdict(self):
+        res = {}
+        res["particle"] = [i.asdict() for i in self._particle_list]
+        res["bond"] = self._bond_dict
+        return res
 
     @property
     def num_particles(self) -> int:
         return self._num_particles
 
     @property
-    def particle_list(self) -> list:
-        return list(self._topology["particle"].keys())
+    def particle_list(self) -> list[Particle]:
+        return self._particle_list
+
+    @property
+    def name_list(self) -> list[str]:
+        return self._name_list
 
     @property
     def num_bonds(self) -> int:
-        return self._num_bonds
+        return len(self.bond_list)
 
     @property
     def bond_list(self) -> list:
-        return list(self._topology["bond"].keys())
+        return list(self._bond_dict.keys())
