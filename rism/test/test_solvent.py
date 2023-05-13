b0c9e9f604c8ea4a214b8ddcb06757ee4ba6f7e8
zhenyuwei99 <zhenyuwei99@gmail.com>
2023-05-13 12:07:43 +0800

    Update test for rism.core.Solvent class

--- rism/test/test_solvent.py
+++ rism/test/test_solvent.py
@@ -9,8 +9,9 @@ copyright : (C)Copyright 2021-present, mdpy organization
 
 import os
 import pytest
-from rism.core import Solvent, solvent
-from rism.potential import FF_DICT
+from rism.core import Solvent
+from rism.error import DuplicatedNameError, SolventIncompleteError
+from rism.element import *
 from rism.unit import *
 
 
@@ -22,92 +23,56 @@ class TestSolvent:
         pass
 
     def test_attribute(self):
-        solvent = Solvent()
+        solvent = Solvent([])
         assert solvent.num_particles == 0
         assert solvent.particle_list == []
         assert solvent.num_bonds == 0
         assert solvent.bond_list == []
 
     def test_exceptions(self):
-        solvent = Solvent()
+        with pytest.raises(DuplicatedNameError):
+            solvent = Solvent([hydrogen(), hydrogen()])
+
+        with pytest.raises(SolventIncompleteError):
+            solvent = Solvent([oxygen("o"), hydrogen("h1"), hydrogen("h2")])
+            solvent.add_bond("o", "h1", Quantity(0.1, nanometer))
+            solvent.check_completeness()
+
+    def test_parse_index(self):
+        solvent = Solvent([oxygen("o"), hydrogen("h1"), hydrogen("h2")])
+        assert solvent._parse_index("o") == 0
+        assert solvent._parse_index(solvent.particle_list[1]) == 1
+        assert solvent._parse_index(2) == 2
+
+        with pytest.raises(ValueError):
+            solvent._parse_index("a")
+
+        with pytest.raises(ValueError):
+            solvent._parse_index(oxygen("o1"))
+
+        with pytest.raises(IndexError):
+            solvent._parse_index(4)
 
         with pytest.raises(KeyError):
-            solvent.get_particle("a")
-
-        with pytest.raises(KeyError):
-            solvent.add_bond("a", "b", 1.0)
-
-        with pytest.raises(KeyError):
-            solvent.get_bond("a", "b")
-
-    def test_add_particles(self):
-        solvent = Solvent()
-
-        solvent.add_particle(name="O", particle_type="o")
-        assert solvent.num_particles == 1
-        solvent.add_particle(name="O", particle_type="o")
-        assert solvent.num_particles == 1
-
-        particle = solvent.get_particle("O")
-        params = FF_DICT["o"]
-        assert particle["type"] == "o"
-        assert particle["sigma"] == check_quantity_value(
-            params["sigma"], default_length_unit
-        )
-        assert particle["epsilon"] == check_quantity_value(
-            params["epsilon"], default_energy_unit
-        )
-        assert particle["q"] == check_quantity_value(params["q"], default_charge_unit)
-
-        solvent.add_particle(
-            name="O",
-            particle_type="o",
-            particle_params={"sigma": 1, "epsilon": 0, "q": 1},
-        )
-        particle = solvent.get_particle("O")
-        assert solvent.num_particles == 1
-        assert particle["sigma"] == 1
-        assert particle["epsilon"] == 0
-        assert particle["q"] == 1
+            solvent._parse_index(None)
 
     def test_add_bond(self):
-        solvent = Solvent()
-        solvent.add_particle(name="O", particle_type="o")
-        solvent.add_particle(name="H1", particle_type="h")
-        solvent.add_bond("O", "H1", Quantity(0.1, nanometer))
+        solvent = Solvent([oxygen("o"), hydrogen("h1"), hydrogen("h2")])
+        solvent.add_bond("o", "h1", Quantity(0.1, nanometer))
 
-        bond = solvent.get_bond("O", "H1")
+        bond = solvent.get_bond_length("o", "h1")
         assert bond == 1
 
-        bond = solvent.get_bond("H1", "O")
+        bond = solvent.get_bond_length("h1", "o")
         assert bond == 1
 
-    def test_io(self):
-        cur_dir = os.path.dirname(os.path.abspath(__file__))
-        solvent_file_path = os.path.join(cur_dir, "out/test_solvent.json")
-
-        solvent = Solvent()
-        solvent.add_particle(name="O", particle_type="o")
-        solvent.add_particle(name="H1", particle_type="h")
-        solvent.add_bond("O", "H1", Quantity(0.1, nanometer))
-
-        solvent.save(solvent_file_path)
-        solvent_load = Solvent(solvent_file_path)
-
-        assert solvent.num_particles == solvent_load.num_particles
-        assert solvent.num_bonds == solvent_load.num_bonds
-        for particle in ["O", "H1"]:
-            particle = solvent.get_particle("O")
-            particle_new = solvent_load.get_particle("O")
-            assert particle["type"] == particle_new["type"]
-            assert particle["epsilon"] == particle_new["epsilon"]
-            assert particle["sigma"] == particle_new["sigma"]
-            assert particle["q"] == particle_new["q"]
-        bond = solvent.get_bond("H1", "O")
-        bond_load = solvent.get_bond("O", "H1")
-        assert bond == bond_load
+        bond = solvent.get_bond_length(1, 0)
+        assert bond == 1
 
 
 if __name__ == "__main__":
-    test = TestSolvent()
-    test.test_add_bond()
+    solvent = Solvent([oxygen("o"), hydrogen("h1"), hydrogen("h2")])
+    solvent.add_bond("o", "h1", 0.9572)
+    solvent.add_bond("o", "h2", 0.9572)
+    solvent.add_bond("h1", "h2", 1.5139)
+    print(solvent.asdict())
